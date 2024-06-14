d40b5d98fa66f4dbcdc357a65a2c44a8eac0c787
Cleber Junio <juninhosete@yahoo.com.br>
2024-06-14 14:10:17 -0300

    feat: add route update task with patch

--- index.js
+++ index.js
@@ -63,6 +63,38 @@ app.get("/tasks/:id", async (req, res) => {
     }
 });
 
-app.listen(8001, () => {
-    console.log("Listening on port 8001...");
+app.patch("/tasks/:id", async (req, res) => {
+    try {
+        const taskId = req.params.id;
+        const taskData = req.body;
+
+        /* pega a tarefa */
+        const taskToUpdate = await TaskModel.findById(taskId);
+
+        /* mapeando os campos permitidos para atualizar tarefa */
+        const allowedUpdates = ["isCompleted"];
+
+        /* pegou os campos que o usuário está querendo atualizar */
+        const requestUpdates = Object.keys(req.body);
+
+        /* pra cada campo que está sendo recebido no body verificar se a lista de campos permitidos pra atualizar 
+        inclui este campo.Se incluir(if) ,significa que este campo pode ser atualizado */
+        for (update of requestUpdates) {
+            if (allowedUpdates.includes(update)) {
+                taskToUpdate[update] = taskData[update];
+            } else {
+                return res
+                    .status(400)
+                    .send("Um ou mais campos inseridos não pode ser editados");
+            }
+        }
+        await taskToUpdate.save();
+        return res.status(200).send(taskToUpdate);
+    } catch (error) {
+        return res.status(500).send(error.message);
+    }
+});
+
+app.listen(4000, () => {
+    console.log("Listening on port 4000...");
 });
