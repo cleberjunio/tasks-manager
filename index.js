84d5c6aec12b5b95fd380e8796e67ae66309bcfe
Cleber Junio <juninhosete@yahoo.com.br>
2024-06-28 14:16:05 -0300

    feat: listening to enviroment port

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ app.use(express.json());
 connectToDatabase();
 app.use("/tasks", TaskRouter);
 
-app.listen(4000, () => {
-    console.log("Listening on port 4000...");
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
 });
